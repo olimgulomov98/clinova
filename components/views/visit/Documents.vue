aa3b4a32e4a9227a5a4dc2f6bada26a51f96d790
olimgulomov98 <olimgulomov98@gmail.com>
2025-07-22 12:56:18 +0500

    fix: update attachment file in patient documents tab

--- components/views/visit/Documents.vue
+++ components/views/visit/Documents.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="page-container mt-5">
     <VTable
-        :table-data="tableDataBySearch"
-        :loading="isLoading"
-        search-left-position
-        @search="search"
+      :table-data="tableDataBySearch"
+      :loading="isLoading"
+      search-left-position
+      @search="search"
     >
       <template #columns>
         <el-table-column prop="action" :label="t('ACTION')">
@@ -15,11 +15,18 @@
               </button>
               <template #dropdown>
                 <el-dropdown-menu class="!p-0">
-                  <el-dropdown-item @click="downloadFile(row?.attachmentLink, row?.originalName)">
+                  <el-dropdown-item
+                    @click="
+                      downloadFile(
+                        `https://${row?.attachmentLink}`,
+                        row?.originalName
+                      )
+                    "
+                  >
                     <button
-                        class="text-base flex gap-2 items-center font-medium text-gray-400 pb-0 justify-between w-full"
+                      class="text-base flex gap-2 items-center font-medium text-gray-400 pb-0 justify-between w-full"
                     >
-                     {{t('DOWNLOAD')}}
+                      {{ t("DOWNLOAD") }}
                     </button>
                   </el-dropdown-item>
                 </el-dropdown-menu>
@@ -27,10 +34,14 @@
             </el-dropdown>
           </template>
         </el-table-column>
-        <el-table-column prop="originalName" :label="t('FILE_NAME')"  />
+        <el-table-column prop="originalName" :label="t('FILE_NAME')" />
         <el-table-column prop="description" :label="t('FILE_DESCRIPTION')" />
-        <el-table-column prop="creationDate" :label="t('UPLOAD_DATE')"  :formatter="(row) => getFormatDate(row?.creationDate)" />
-        <el-table-column prop="createdBy" :label="t('UPLOADED_BY')"  />
+        <el-table-column
+          prop="creationDate"
+          :label="t('UPLOAD_DATE')"
+          :formatter="(row) => getFormatDate(row?.creationDate)"
+        />
+        <el-table-column prop="createdBy" :label="t('UPLOADED_BY')" />
         <el-table-column prop="visit" :label="t('VISIT_NUMBER')" />
       </template>
     </VTable>
@@ -46,9 +57,8 @@ const isLoading = ref(false);
 const loading = ref(false);
 const route = useRoute();
 
-
 const filters = ref<any>({
-  searchKey: null
+  searchKey: null,
 });
 
 const tableData = ref<any>([]);
@@ -56,8 +66,9 @@ const tableData = ref<any>([]);
 const getData = async () => {
   isLoading.value = true;
   try {
-
-    const response = await (<Axios>$axios).get(`/api/attachment/patient/${route.params.patientId}`);
+    const response = await (<Axios>$axios).get(
+      `/api/attachment/patient/${route.params.patientId}`
+    );
     const data = response?.data?.payload;
 
     if (data) {
@@ -65,6 +76,8 @@ const getData = async () => {
     } else {
       console.warn("No payload found in response:", response);
     }
+
+    console.log(`Fayl:`, data);
   } catch (error: any) {
     console.error("Failed to fetch data:", error?.message || error);
   } finally {
@@ -72,13 +85,26 @@ const getData = async () => {
   }
 };
 
-function downloadFile(url, filename) {
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = filename || '';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+async function downloadFile(url: string, filename: string) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Fetch failed: ${response.status}`);
+    }
+
+    const blob = await response.blob();
+    const downloadUrl = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = downloadUrl;
+    a.download = filename || "file";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(downloadUrl);
+  } catch (err) {
+    console.error("Download xatoligi:", err);
+  }
 }
 
 const search = (value: string) => {
@@ -91,21 +117,24 @@ const tableDataBySearch = computed(() => {
   const searchKey = filters.value.searchKey?.toLowerCase()?.trim();
   if (!searchKey) return tableData.value;
 
-  return tableData.value?.filter(item => {
+  return tableData.value?.filter((item) => {
     return [
       item.attachmentLink,
       item.originalName,
       item.description,
       item.createdBy,
-      item.visit
-    ].some(field => field?.toLowerCase()?.includes(searchKey));
+      item.visit,
+    ].some((field) => field?.toLowerCase()?.includes(searchKey));
   });
 });
-
 
 onMounted(() => {
   getData();
 });
+
+function formatBytes(arg0: number) {
+  throw new Error("Function not implemented.");
+}
 </script>
 
 <style scoped lang="scss">
