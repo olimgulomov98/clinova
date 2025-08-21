86c8c77b86e5e3b71e111bbcd42be2397efc389d
olimgulomov98 <olimgulomov98@gmail.com>
2025-08-21 14:19:55 +0500

    fix: update phone number in create patient page

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
 import type { LocationQueryValueRaw } from "vue-router";
 import dayjs from "dayjs";
-import { ElNotification } from 'element-plus';
-import parseZone from 'dayjs/plugin/timezone'
+import { ElNotification } from "element-plus";
+import parseZone from "dayjs/plugin/timezone";
 
-dayjs.extend(parseZone)
-export function normalizeQueryParams(query: Record<string, unknown>): Record<string, unknown> {
+dayjs.extend(parseZone);
+export function normalizeQueryParams(
+  query: Record<string, unknown>
+): Record<string, unknown> {
   const normalized: Record<string, unknown> = {};
 
   for (const key in query) {
@@ -24,19 +26,39 @@ export function normalizeQueryParams(query: Record<string, unknown>): Record<str
   return normalized;
 }
 
-export function getDiffKeys(obj1: Record<string, unknown>, obj2: Record<string, unknown>) {
-  return Object.keys({ ...obj1, ...obj2 }).filter((key) => obj1[key] !== obj2[key]);
+export function getDiffKeys(
+  obj1: Record<string, unknown>,
+  obj2: Record<string, unknown>
+) {
+  return Object.keys({ ...obj1, ...obj2 }).filter(
+    (key) => obj1[key] !== obj2[key]
+  );
 }
 
-export const removeKeys = (obj: Record<string, LocationQueryValueRaw | LocationQueryValueRaw[]>, keysToRemove: object) => {
-  return Object.fromEntries(Object.entries(obj).filter(([key]) => !Object.keys(keysToRemove).includes(key)));
+export const removeKeys = (
+  obj: Record<string, LocationQueryValueRaw | LocationQueryValueRaw[]>,
+  keysToRemove: object
+) => {
+  return Object.fromEntries(
+    Object.entries(obj).filter(
+      ([key]) => !Object.keys(keysToRemove).includes(key)
+    )
+  );
 };
 
-export function clean(obj: Record<string, unknown>): Record<string, LocationQueryValueRaw | LocationQueryValueRaw[]> {
-  const cleanedObj: Record<string, LocationQueryValueRaw | LocationQueryValueRaw[]> = {};
+export function clean(
+  obj: Record<string, unknown>
+): Record<string, LocationQueryValueRaw | LocationQueryValueRaw[]> {
+  const cleanedObj: Record<
+    string,
+    LocationQueryValueRaw | LocationQueryValueRaw[]
+  > = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    if (value !== null && value !== undefined && value !== "") cleanedObj[key] = value  as LocationQueryValueRaw | LocationQueryValueRaw[];;
+    if (value !== null && value !== undefined && value !== "")
+      cleanedObj[key] = value as
+        | LocationQueryValueRaw
+        | LocationQueryValueRaw[];
   }
   return cleanedObj;
 }
@@ -55,55 +77,86 @@ export const convertToFormData = (data: Record<string, unknown>): FormData => {
       data[key] !== "" &&
       (!Array.isArray(data[key]) || data[key].length > 0)
     ) {
-      if (Array.isArray(data[key]) && data[key].length > 0 && data[key][0] instanceof File) {
-        console.log('1')
-        data[key].forEach((file:File) => {
-          formData.append('files', file);
-        })
-      }
-      else if (data[key] instanceof Blob || data[key] instanceof File) {
-        console.log('2')
+      if (
+        Array.isArray(data[key]) &&
+        data[key].length > 0 &&
+        data[key][0] instanceof File
+      ) {
+        console.log("1");
+        data[key].forEach((file: File) => {
+          formData.append("files", file);
+        });
+      } else if (data[key] instanceof Blob || data[key] instanceof File) {
+        console.log("2");
         formData.append(key, data[key] as Blob);
-      }
-       else {
-        console.log('3')
+      } else {
+        console.log("3");
         formData.append(key, String(data[key]));
       }
-
     }
   }
   return formData;
 };
 
-export const getFormatAmount = (number:number) => {
-  if(!number) return 0;
-  return  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
+export const getFormatAmount = (number: number) => {
+  if (!number) return 0;
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
 
-import { useLanguages } from '@/composables/useLanguages';
+import { useLanguages } from "@/composables/useLanguages";
 
 const monthNames: Record<string, string[]> = {
   en: [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ],
   ru: [
-    'января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
-    'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
   ],
   uz: [
-    'yanvar', 'fevral', 'mart', 'aprel', 'may', 'iyun',
-    'iyul', 'avgust', 'sentyabr', 'oktyabr', 'noyabr', 'dekabr'
-  ]
+    "yanvar",
+    "fevral",
+    "mart",
+    "aprel",
+    "may",
+    "iyun",
+    "iyul",
+    "avgust",
+    "sentyabr",
+    "oktyabr",
+    "noyabr",
+    "dekabr",
+  ],
 };
 
 export const getFormatDate = (input: string) => {
   if (!input) return;
 
-  const  currentLanguage  = localStorage.getItem('lang'); // 'en', 'ru', 'uz'
-  const lang = monthNames[currentLanguage] ? currentLanguage : 'en'; // fallback to English
+  const currentLanguage = localStorage.getItem("lang"); // 'en', 'ru', 'uz'
+  const lang = monthNames[currentLanguage] ? currentLanguage : "en"; // fallback to English
 
-  const date = new Date(input.split('.')[0]);
+  const date = new Date(input.split(".")[0]);
 
   const day = date.getDate();
   const year = date.getFullYear();
@@ -114,22 +167,23 @@ export const getFormatDate = (input: string) => {
   return `${day} ${monthName} ${year}`;
 };
 
-
-
-export const getFormatDateTime= (input:string) => {
-  if(!input) return ;
-  const date = new Date(input.split('.')[0]);
+export const getFormatDateTime = (input: string) => {
+  if (!input) return;
+  const date = new Date(input.split(".")[0]);
   let hours = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
 
   hours = hours % 12;
   hours = hours ? hours : 12;
 
   return `${hours}:${minutes} ${ampm}`;
-}
+};
 
-export const notificationShower = (type: 'success' | 'warning' | 'info' | 'error', message: string) => {
+export const notificationShower = (
+  type: "success" | "warning" | "info" | "error",
+  message: string
+) => {
   ElNotification({
     message: message,
     type: type,
@@ -137,11 +191,11 @@ export const notificationShower = (type: 'success' | 'warning' | 'info' | 'error
 };
 
 export function cleanPhoneNumber(phone: string): string {
-  return '+'+phone.replace(/\D/g, '');
+  return phone.replace(/\D/g, "");
 }
 
 export function formatAutoDate(value: string): string {
   return value.length === 10
-    ? dayjs(value).format('D MMM')   // "9 Jul"
-    : dayjs(value + '-01').format('YYYY MMM') // "Jul"
+    ? dayjs(value).format("D MMM") // "9 Jul"
+    : dayjs(value + "-01").format("YYYY MMM"); // "Jul"
 }
